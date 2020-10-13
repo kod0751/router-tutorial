4b5a5301c36a62970391dae6db16ff64799b129f
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-10-13 23:42:33 +0900

    Link, 파라미터, 쿼리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to = '/'>홈</Link>
+        </li>
+        <li>
+          <Link to = '/about'>소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact />
-      <Route path="/about" component={About}/>
+      <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
